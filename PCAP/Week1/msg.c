b2888dd0f1eb0c91767fbd244634d134f91a04f1
RYXVL <95095676+RYXVL@users.noreply.github.com>
2023-01-30 09:49:40 +0530

    Update msg.c

--- PCAP/Week1/msg.c
+++ PCAP/Week1/msg.c
@@ -7,9 +7,9 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if(rank % 2 == 0)
-        printf("Hello from the even rank process - %d.\n", rank);
+        printf("Hello - %d.\n", rank);
     else
-        printf("Hello from the odd rank process - %d.\n", rank);
+        printf("World - %d.\n", rank);
     MPI_Finalize();
     return 0;
 }
