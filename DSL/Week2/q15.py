cd9672b70a38b2cb4715e08af46188800d3cc9e3
RYXVL <95095676+RYXVL@users.noreply.github.com>
2023-02-09 16:00:45 +0530

    Create q15.py

---
+++ DSL/Week2/q15.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+data = {'Name':['Kavitha', 'Sudha', 'Raju', 'Vignesh'],'Height':[28, 34, 29, 42], 'Qualification': ['B.Tech', 'MBA', 'MBBS', 'BE']}
+df = pd.DataFrame(data)
+print(df)
+print('\n')
+address = ['abc', 'def', 'ghi', 'jkl']
+df['Adress'] = address
+print(df)
